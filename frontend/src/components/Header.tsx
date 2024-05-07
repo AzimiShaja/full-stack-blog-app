cc34b66d6f4c5d13ed034d7d834e2957d2ba53f3
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-07 11:30:37 +0300

    added design to header logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,13 +2,13 @@ import { GiTeamIdea } from "react-icons/gi";
 const Header = () => {
     return (
         <div className="flex justify-around shadow-md items-center max-md:justify-between p-5 gap-5">
-            <h1 className="text-3xl cursor-pointer hover:text-purple-700 transition-all font-bold flex items-center justify-center gap-2">
+            <h1 className="text-3xl font-pacifico cursor-pointer hover:text-purple-700 transition-all font-bold flex items-center justify-center gap-2">
                 <GiTeamIdea size={40} />
-                GotIdea
+                gotIdea
             </h1>
             <nav>
                 <ul className="flex gap-5 nav-links max-md:hidden">
-                    <li>What Is GotIdea?</li>
+                    <li>What Is gotIdea?</li>
                     <li>FAQ</li>
                     <li>Report an Issue</li>
                 </ul>
