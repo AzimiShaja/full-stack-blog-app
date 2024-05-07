600c073c7e10e7438c368533da251dc0e3ee01b9
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-07 11:31:11 +0300

    made textFields outline purple

--- frontend/src/components/forms/Login.tsx
+++ frontend/src/components/forms/Login.tsx
@@ -1,18 +1,33 @@
 import { Button, TextField } from "@mui/material";
 import { useContext } from "react";
 import { AuthStatus } from "../context/AuthContext";
-
+import image from "../../assets/User.gif";
 const Login = () => {
     const { setIsToLogin } = useContext(AuthStatus);
     return (
-        <div className="h-[calc(100vh-100px)] flex items-center justify-center">
-            <form className="flex flex-col  gap-3 shadow-xl p-10 border border-gray-300 rounded-lg lg:min-w-[500px]">
+        <div className="h-[calc(100vh-100px)] flex items-center justify-center max-md:px-8">
+            <form className="flex flex-col  gap-3 shadow-xl p-10 border border-gray-300 rounded-lg md:min-w-[500px] max-md:w-full">
                 <div className="w-full flex flex-col items-center gap-1 mb-10">
-                    <h1 className="text-2xl font-bold">Account Login</h1>
-                    <h1 className="text-md font-light text-gray-400">Sign in to your account</h1>
+                    <img src={image} className="w-20 h-20" alt="" />
+                    <h1 className="md:text-2xl text-xl font-bold">Welcome to GotIdea!</h1>
+                    <h1 className="md:text-md text-sm font-light text-gray-400">
+                        Please login to your account
+                    </h1>
                 </div>
-                <TextField id="outlined-basic" size="small" label="Email address" variant="outlined" />
-                <TextField id="outlined-basic" size="small" label="Password" variant="outlined" />
+                <TextField
+                    id="outlined-basic"
+                    size="small"
+                    label="Email address"
+                    variant="outlined"
+                    color="secondary"
+                />
+                <TextField
+                    id="outlined-basic"
+                    size="small"
+                    label="Password"
+                    variant="outlined"
+                    color="secondary"
+                />
                 <Button type="submit" color="secondary" variant="contained">
                     Login
                 </Button>
--- frontend/src/components/forms/Signup.tsx
+++ frontend/src/components/forms/Signup.tsx
@@ -1,18 +1,38 @@
 import { Button, TextField } from "@mui/material";
 import { useContext } from "react";
 import { AuthStatus } from "../context/AuthContext";
+import image from "../../assets/User.gif";
 const Signup = () => {
     const { setIsToLogin } = useContext(AuthStatus);
     return (
         <div className="h-[calc(100vh-80px)] flex items-center justify-center max-md:px-8">
             <form className="flex flex-col bg-white gap-3 shadow-xl p-10 border border-gray-300 rounded-lg md:min-w-[500px] max-md:w-full">
                 <div className="w-full flex flex-col items-center gap-1 mb-10">
-                    <h1 className="text-2xl font-bold">Account Creation</h1>
-                    <h1 className="text-md font-light text-gray-400">Sign Up your account</h1>
+                    <img src={image} className="w-20 h-20" alt="" />
+                    <h1 className="md:text-2xl text-xl font-bold">Account Creation</h1>
+                    <h1 className="md:text-md text-sm font-light text-gray-400">Sign Up your account</h1>
                 </div>
-                <TextField id="outlined-basic" size="small" label="Full Name" variant="outlined" />
-                <TextField id="outlined-basic" size="small" label="Email address" variant="outlined" />
-                <TextField id="outlined-basic" size="small" label="Password" variant="outlined" />
+                <TextField
+                    id="outlined-basic"
+                    size="small"
+                    label="Full Name"
+                    variant="outlined"
+                    color="secondary"
+                />
+                <TextField
+                    id="outlined-basic"
+                    size="small"
+                    label="Email address"
+                    variant="outlined"
+                    color="secondary"
+                />
+                <TextField
+                    id="outlined-basic"
+                    size="small"
+                    label="Password"
+                    variant="outlined"
+                    color="secondary"
+                />
                 <Button type="submit" color="secondary" variant="contained">
                     Create Account
                 </Button>
