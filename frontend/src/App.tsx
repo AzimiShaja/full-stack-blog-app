c9b0a4627c6f595566414cdf6144102623be40fc
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-07 21:34:53 +0300

    added check function to find correct path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,27 @@ import Header from "./components/Header";
 import { useContext } from "react";
 import Login from "./components/forms/Login";
 import { AuthStatus } from "./components/context/AuthContext";
+import Home from "./components/routes/Home";
 
 const App = () => {
+  const { isToLogin, isUserLoggedIn } = useContext(AuthStatus);
+  const findPath = () => {
+    if (isUserLoggedIn) {
+      return <Home />;
+    } else {
+      if (isToLogin) {
+        return <Login />;
+      } else {
+        return <Signup />;
+      }
+    }
+  };
 
-  const { isToLogin } = useContext(AuthStatus);
   return (
     <div>
       <Header />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              {isToLogin ? <Login /> : <Signup />}
-            </div>
-          }
-        />
+        <Route path="/" element={<div>{findPath()}</div>} />
       </Routes>
     </div>
   );
