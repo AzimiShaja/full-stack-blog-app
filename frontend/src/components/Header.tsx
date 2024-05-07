f675200fb69c3f2db4825dd2c5681cfbb02d0e58
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-07 20:17:54 +0300

    added about dialog

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,8 @@
 import { GiTeamIdea } from "react-icons/gi";
+import About from "./Dialogs/About";
+import { useState } from "react";
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="flex justify-around shadow-md items-center max-md:justify-between p-5 gap-5">
             <h1 className="text-3xl font-pacifico cursor-pointer hover:text-purple-700 transition-all font-bold flex items-center justify-center gap-2">
@@ -8,11 +11,12 @@ const Header = () => {
             </h1>
             <nav>
                 <ul className="flex gap-5 nav-links max-md:hidden">
-                    <li>What Is gotIdea?</li>
+                    <li onClick={() => setIsOpen(true)}>What Is gotIdea?</li>
                     <li>FAQ</li>
                     <li>Report an Issue</li>
                 </ul>
             </nav>
+            <About isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
     );
 };
