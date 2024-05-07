ad534818be767148285121e43658afb9f76a92fb
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-07 11:10:35 +0300

    removed unnecessary useState import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "./components/forms/Signup";
 import Header from "./components/Header";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import Login from "./components/forms/Login";
 import { AuthStatus } from "./components/context/AuthContext";
 
