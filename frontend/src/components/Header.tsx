465f98865c75d2a18989cb3cb696af69f5333064
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-05-08 11:24:35 +0300

    designed header based on user status

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,67 @@
 import { GiTeamIdea } from "react-icons/gi";
 import About from "./Dialogs/About";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthStatus } from "./context/AuthContext";
+import { Avatar, Tab, Tabs, Tooltip } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { IoCreateOutline } from "react-icons/io5";
+import SearchIcon from "@mui/icons-material/Search";
+import av1 from "../assets/avatars/av1.jpg";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+    const { isUserLoggedIn } = useContext(AuthStatus);
     const [isOpen, setIsOpen] = useState(false);
+    const [tab, setTab] = useState(0);
+    const navigate = useNavigate();
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        event;
+        setTab(newValue);
+    };
+
     return (
         <div className="flex justify-around items-center max-md:justify-between p-5 gap-5">
             <h1 className="text-3xl font-pacifico cursor-pointer hover:text-purple-700 transition-all font-bold flex items-center justify-center gap-2">
                 <GiTeamIdea size={40} />
-                gotIdea
+                {isUserLoggedIn ? "" : "gotIdea"}
             </h1>
-            <nav>
-                <ul className="flex gap-5 nav-links max-md:hidden">
-                    <li onClick={() => setIsOpen(true)}>What Is gotIdea?</li>
-                    <li>FAQ</li>
-                    <li>Report an Issue</li>
-                </ul>
-            </nav>
+
+            {isUserLoggedIn ? (
+                <Tabs indicatorColor="secondary" textColor="secondary" value={tab} onChange={handleChange}>
+                    <Tooltip title="Home">
+                        <Tab icon={<HomeIcon />} aria-label="home" onClick={() => navigate("/")} />
+                    </Tooltip>
+                    <Tooltip title="Search">
+                        <Tab icon={<SearchIcon />} aria-label="person" onClick={() => navigate("/search")} />
+                    </Tooltip>
+                    <Tooltip title="Create">
+                        <Tab
+                            icon={<IoCreateOutline size={25} className="mb-1" />}
+                            onClick={() => navigate("/createpost")}
+                            aria-label="create"
+                        />
+                    </Tooltip>
+                    <Tooltip title="Favorites">
+                        <Tab
+                            icon={<FavoriteIcon />}
+                            onClick={() => navigate("/favorites")}
+                            aria-label="favorite"
+                        />
+                    </Tooltip>
+                </Tabs>
+            ) : (
+                <nav>
+                    <ul className="flex gap-5 nav-links max-md:hidden">
+                        <li onClick={() => setIsOpen(true)}>What Is gotIdea?</li>
+                        <li>FAQ</li>
+                        <li>Report an Issue</li>
+                    </ul>
+                </nav>
+            )}
             <About isOpen={isOpen} setIsOpen={setIsOpen} />
+            {isUserLoggedIn && (
+                <Avatar src={av1} alt="avatar" sx={{ width: 35, height: 35 }} className="cursor-pointer" />
+            )}
         </div>
     );
 };
